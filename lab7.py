84508d9d8b46d6dc9159c85722f190bb11d23a75
Brent <pnxrock101@gmail.com>
2021-04-09 12:27:51 -0700

    beginning of function build

---
+++ lab7.py
@@ -0,0 +1,12 @@
+import os
+import netmiko
+import datetime
+
+user = 'username'
+pw = 'password'
+date = datetime.date.today()
+
+def ciscoRunConf:
+    user = input('Please enter your username: ')
+    pw = getpass('Please enter your password: ')
+    
