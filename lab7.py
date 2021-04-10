5b4031b5fe1f5024148af1279cad53be5205c2fe
brent <pnxrock101@gmail.com>
2021-04-10 08:40:18 -0700

    function 1st draft complete

--- lab7.py
+++ lab7.py
@@ -5,8 +5,18 @@ import datetime
 user = 'username'
 pw = 'password'
 date = datetime.date.today()
+netDevice = {
+    'ip': 'ip_address',
+    'device_type': 'cisco_ios',
+    'username': user,
+    'password': pw
+    }
+
 
 def ciscoRunConf:
     user = input('Please enter your username: ')
     pw = getpass('Please enter your password: ')
+    net_connect = ConnectHandler(**netDevice)
+    output = net_connect.send_command("show run")
+    print(output, file=open("%s.txt" % date, "a+")
     
