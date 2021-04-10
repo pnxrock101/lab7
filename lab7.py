6e512bdf5e18f5346b7f63d6288d6f0613f9ae47
brent <pnxrock101@gmail.com>
2021-04-10 10:00:10 -0700

    Funtion 3rd draft

--- lab7.py
+++ lab7.py
@@ -1,22 +1,27 @@
-import os
-import netmiko
+from netmiko import ConnectHandler
 import datetime
 import getpass
+import os
 
 user = 'username'
 pw = 'password'
 date = datetime.date.today()
+ip_addr = ("192.168.108.129")
+
 netDevice = {
-    'ip': 'ip_address',
+    'ip': ip_addr,
     'device_type': 'cisco_ios',
     'username': user,
-    'password': pw
+    'password': pw,
+    'use_keys': True,
+    'key_file': '/home/sdnstudent/lab7/lab7/rsa_key1',
+    'port': 22,
     }
 
 
 def ciscoRunConf():
     user = input('Please enter your username: ')
-    pw = getpass('Please enter your password: ')
+    pw = getpass.getpass(prompt='Please enter your password: ')
     net_connect = ConnectHandler(**netDevice)
     output = net_connect.send_command("show run")
     print(output, file=open("%s.txt" % date, "a+"))
